e86db6485c78354d84c2e703f9dd389de3ca126f
Vinicius Boschi <vinicius.yg@gmail.com>
2022-06-24 19:08:47 -0300

    Adicionando mudanças finais no css e no js

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# ClimaTempo
+<h3> :dart: Objetivos do site</h3>
+Site para se informar sobre o clima de sua cidade.
+  
+# <h3> :pencil: Tela Principal</h3>  
+![climatempo](https://user-images.githubusercontent.com/74377158/175696881-b1f587f8-de4f-4083-a5a9-5bd0cef0c274.jpg)
+
+# <h3> :file_folder: Acessar o projeto</h3>
+No terminal, clone o projeto:
+
+```
+git clone https://github.com/Vinicius-Boschi/ClimaTempo.git
+```
+
+# <h3> :wrench: Abrir e rodar o projeto</h3>
+Com o projeto clonado, você irá precisar de uma chave da api que você pode conseguir [aqui](https://openweathermap.org/api).
+Com a chave você irá por ela no arquivo <strong>apikeyExample.js</strong>
+
+# <h3> :heavy_check_mark: Status do Projeto:</h3>
+![Badge em Desenvolvimento](https://img.shields.io/static/v1?label=STATUS&message=CONCLUIDO&color=blue&style=for-the-badge)
+
+# <h3> :hammer: Funcionalidades do projeto</h3>
+- `Funcionalidade 1`: Pesquisa de cidade.
+- `Funcionalidade 2`: Mostra as informações do clima.
+
+# <h3> :notebook_with_decorative_cover: Tecnologia usadas:</h3>
+
+* <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" alt="javascript"><br>
+* <img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" alt="html"><br>
+* <img src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white" alt="css"><br>
+* <img src="https://img.shields.io/badge/Sass-CC6699?style=for-the-badge&logo=sass&logoColor=white" alt="sass">
+
+# <h3> :computer: Desenvolvedores:</h3>
+[<img src="https://user-images.githubusercontent.com/74377158/173900850-b6afcc77-36a5-4254-b63f-983397918d54.jpg" width=130><br><sub>Vinícius Boschi</sub>](https://github.com/Vinicius-Boschi)
+
+# <h3> :bangbang: Licença:</h3>
+<p> Todos os direitos reservados :copyright: 2022 - ClimaTempo
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,65 @@
-let api_key = '92adc081'
+import { api_key } from "../../apikey.js"
 
 const input = document.querySelector("input")
 const searchButton = document.querySelector(".main__icon")
-// const container = document.querySelector(".main__container")
 
-searchButton.addEventListener('click', searchCity)
-input.addEventListener('keyup', function(event) {
-    if (event.keyCode == 13) {
-        searchCity()
-        return
+let results = {
+    lang: 'pt_br',
+    fetchResults: function(city) { 
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + `${api_key}` + '&lang=' + this.lang) 
+        .then((response) => {
+            if (!response.ok) { 
+                alert("Nenhuma informação encontrada!")
+                throw new Error("Nenhuma informação encontrada!")
+            }
+            return response.json()
+        })
+        .then((data) => this.displayResults(data))
+    },
+
+    displayResults: function(data, day) { 
+        const { name } = data 
+        const { icon, description } = data.weather[0]
+        const { temp, humidity , temp_min, temp_max} = data.main
+        const { speed } = data.wind
+        const { country, sunrise, sunset} = data.sys
+        let now = new Date()
+        date.textContent = date(now)
+
+        document.querySelector(".main__title").textContent = `${name}, ${country}`
+        document.querySelector(".main__img").src = "https://openweathermap.org/img/wn/" + icon + ".png"
+        document.querySelector(".main__description").textContent = description
+        document.querySelector(".main__temp").textContent = temp.toFixed(0) + "°C"
+        document.querySelector(".main__temp_min").textContent = "Miníma " + temp_min.toFixed(0) + "°C"
+        document.querySelector(".main__temp_max").textContent = "Máxima " + temp_max.toFixed(0) + "°C"
+        document.querySelector(".main__humidity").textContent = "Umidade: " + humidity + "%"
+        document.querySelector(".main__wind").textContent = "Vento: " + speed.toFixed(0) + " km/h"
+        document.querySelector("input").value = ''
+    },
+
+    search: function() { 
+        this.fetchResults(input.value)
+    },
+}
+
+function date(d) {
+    let days = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    let months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+
+    let day = days[d.getDay()]
+    let date = d.getDate()
+    let month = months[d.getMonth()]
+    let year = d.getFullYear()
+
+    document.querySelector(".main__date").textContent = `${day}, ${date}/${month}/${year}`
+}
+
+searchButton.addEventListener('click', search) 
+input.addEventListener('keyup', function(event) { 
+    if (event.keyCode == 13) { 
+        results.search()
+        return 
     }
 })
 
-async function searchCity() {
-    const inputValue = input.value
-    console.log(inputValue)
-    if (inputValue != '') { 
-        const getCitys = await searchCityByName(inputValue)
-        Array.from(getCitys).forEach(city => renderCity(allCitys))
-    }
-}
-
-async function searchCityByName(city_name) {
-    const url = `https://api.hgbrasil.com/weather?format=json-cors&key=${api_key}&city_name=${city_name}`
-    const fetchResponse = await fetch(url)
-    const { results } = await fetchResponse.json()
-    console.log(results)
-    return results
-}
-
-async function getClimate() {
-    const url = `https://api.hgbrasil.com/weather?format=json-cors&key=${api_key}`
-    const fetchResponse = await fetch(url)      
-    const { results } = await fetchResponse.json()
-    return results
-}
-
-async function getAllCitys() {
-    const getCitys = await getClimate() 
-    Array.from(getCitys).forEach(city => renderCity(allCitys))
-}
-
-window.onload = function() {
-    getAllCitys()
-}
-
-function renderCity(allCitys) {
-    const { temp, description, city , humidity, wind_speedy} = allCitys
-
-    document.querySelector(".main__title").innerHTML = "Temperatura em" + city
-    document.querySelector(".main__description").innerHTML = description
-    document.querySelector(".main__temp").innerHTML = temp + "°C"
-    document.querySelector(".main__humidity").innerHTML = "Humidade: " + humidity + "%"
-    document.querySelector(".main__wind").innerHTML = "Vento: " + wind_speedy + " km/h"
-}
+results.fetchResults("São Paulo")
